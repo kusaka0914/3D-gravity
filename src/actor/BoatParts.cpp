8de420a983c01f3537e9566944e3f9fb11837136
Takumi Kusaka <your.email@example.com>
2026-05-28 03:21:07 +0900

    BoatPartsのリファクタリング、テストが完了しました。

--- src/actor/BoatParts.cpp
+++ src/actor/BoatParts.cpp
@@ -1,27 +1,30 @@
 #include "BoatParts.h"
 #include "Game.h"
-#include "system/AudioSystem.h"
 #include "component/CollectableComponent.h"
+#include "system/AudioSystem.h"
 
-BoatParts::BoatParts(Game* game)
-    : Actor(game)
+BoatParts::BoatParts(Game* game) : Actor(game)
 {
     AddCollectableComponent();
 }
 
-void BoatParts::AddCollectableComponent() {
+void BoatParts::AddCollectableComponent()
+{
     std::unique_ptr<CollectableComponent> collectableComponent = std::make_unique<CollectableComponent>(this, 100);
     mCollectableComponent = collectableComponent.get();
     AddComponent(std::move(collectableComponent));
 }
 
-void BoatParts::UpdateActor(float deltaTime) {
-    bool isObtained = mCollectableComponent->GetIsObtained();
-    if (isObtained && mIsActive)
+void BoatParts::UpdateActor(float deltaTime)
+{
+    bool shouldStartOnObtained = mCollectableComponent->GetIsObtained() && mIsActive;
+    if (shouldStartOnObtained) {
         OnObtained();
+    }
 }
 
-void BoatParts::OnObtained() {
+void BoatParts::OnObtained()
+{
     mIsActive = false;
     mGame->GetAudioSystem()->PlaySE("pickUpSE");
 }
