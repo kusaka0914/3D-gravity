02642e261f8dbb6d0182e6cc93cbf0eab1888fd9
Takumi Kusaka <your.email@example.com>
2026-05-30 04:15:43 +0900

    要点PDFを追加しました。

--- src/state/GameProgressState.cpp
+++ src/state/GameProgressState.cpp
@@ -1,11 +1,7 @@
 #include "GameProgressState.h"
 #include "Game.h"
 
-GameProgressState::GameProgressState(Game* game)
-    : mGame(game),
-      mIsFirstBreak(false),
-      mIsFirstStrongAttack(false),
-      mCurrentSceneState(SceneState::Playing),
-      mNextSceneState(SceneState::None)
-{
+GameProgressState::GameProgressState(Game *game)
+    : mGame(game), mIsFirstBreak(false), mIsFirstStrongAttack(false),
+      mCurrentSceneState(SceneState::Title), mNextSceneState(SceneState::None) {
 }
